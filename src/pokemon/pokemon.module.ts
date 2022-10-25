dee4f27140129814ff2fa52017bed26bc2074b14
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2022-10-25 17:59:42 +0200

    Pokemon schema

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonSchema }])
+  ]
 })
 export class PokemonModule {}
