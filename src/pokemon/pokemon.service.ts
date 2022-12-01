3c93868e766a38ba4932f965072a135c12afe025
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2022-12-01 12:52:27 +0100

    Custom pipes

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -81,10 +81,7 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon: Pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
-
-    return pokemon;
+    return await this.pokemonModel.findByIdAndDelete(id);
   }
 
   private _handleException(error: any) {
