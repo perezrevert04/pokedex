ce4aa9d11f46f387a02ddec249f7f8912d30c43f
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2022-12-02 16:01:37 +0100

    Axios custom provider

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from 'src/common/common.module';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
 import { SeedController } from './seed.controller';
 import { SeedService } from './seed.service';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [PokemonModule, CommonModule],
   controllers: [SeedController],
   providers: [SeedService]
 })
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,22 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
   ) {}
 
   async execute() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650'
-    );
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=650';
+    const data = await this.http.get<PokeResponse>(url);
 
     const pokemonToBeInserted = data.results.map(({ name, url }) => {
       const no = url.split('/')[6];
